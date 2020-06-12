71a2d533d705fe8dcfc7fba25217aaeb8dd8c440
Sauri-Sufian <abusauri.iub@gmail.com>
2020-06-12 10:15:19 +0100

    new

---
+++ controllers/signin.js
@@ -0,0 +1,35 @@
+
+
+const handleSignin=(req,res,db ,bcrypt)=>{
+    const {email,password}=req.body
+    if(!email || !password){
+        res.status(400).json("can't be empty")
+    }
+    else{ 
+    db.select('email','hash').from('login')
+    .where('email','=',email)
+    .then(data=>{
+        const isValid =bcrypt.compareSync(password,data[0].hash);
+        if(isValid){
+          return   db.select('*').from('users')
+            .where('email','=',email)
+            .then(user=>{
+                res.json(user[0])
+            })
+            .catch(err =>{
+                res.status(400).json("unable to signin")
+            })
+        }
+        else{
+            res.json("Wrong info bitch")
+        }
+    })
+    .catch(err=>{
+        res.status(400).json("unable to signin")
+    })
+}
+}
+
+module.exports={
+    handleSignin:handleSignin
+}
