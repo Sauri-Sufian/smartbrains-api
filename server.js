9abc480484cc04327d403535e1a9944f66026cec
Sauri-Sufian <abusauri.iub@gmail.com>
2020-06-12 12:53:50 +0100

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db=knex({
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL ,
+     host: 'postgresql-flexible-82117' ,
           ssl: true
     }
   
