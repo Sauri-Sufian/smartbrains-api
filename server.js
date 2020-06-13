26412ba9975e65e472d300fc872de7d97cea1850
Sauri-Sufian <abusauri.iub@gmail.com>
2020-06-13 14:24:27 -0400

    database connected

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db=knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      { rejectUnauthorized: false },
+      ssl:{ rejectUnauthorized: false },
     }
   });
 let users;
